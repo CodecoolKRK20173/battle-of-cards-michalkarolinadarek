d491faed0e666d35da406af738fa7854814f4614
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-12 19:24:15 +0100

    Add new fields to Card class

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,13 @@
 package cards;
 class Card implements Comparable{
 
+    String name;
+    Integer deaths;
+    Integer incubation;
+    Integer infectvity;
+    Integer painfulness;
+    Integer panicLevel;
+
     @Override
     public int compareTo(Object arg0) {
         // TODO Auto-generated method stub
