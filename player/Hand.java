fbd4cf330614801f8d2722276e0ca02a92433bef
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-13 17:29:41 +0100

    Write sheleton of Hand class

--- player/Hand.java
+++ player/Hand.java
@@ -1,4 +1,20 @@
 package player;
-class Hand{
-    
+
+import java.util.List;
+import cards.Card;
+
+class Hand {
+    List<Card> cardsToUse;
+
+    Hand(){
+
+    }
+
+    void moveToUsedPile(Card card){
+
+    }
+
+    Card removeCard(){
+        return null;
+    }
 }
