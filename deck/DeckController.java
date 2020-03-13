7e23caf651427f9952a835e9e265e4e93d8dde4b
Dariusz Raba <rabadariusz@gmail.com>
2020-03-13 17:15:31 +0100

    Write sheleton of Deck-Controller

--- deck/DeckController.java
+++ deck/DeckController.java
@@ -1,4 +1,20 @@
 package deck;
-class DeckController{
-    
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cards.Card;
+
+class DeckController {
+    List<Card> DAOcards;
+    List<Card> deckOfCards;
+
+    DeckController(DeckDAO daodao){
+    this.DAOcards = daodao.deck;  
+    deckOfCards = new ArrayList<>();      
+    }
+
+    public void createDeckOfCards(){
+            
+    }
 }
