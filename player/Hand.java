82f0d93bdec4f9a56a2b311a7effc631727c037e
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-14 12:07:27 +0100

    Implement method in Hand

--- player/Hand.java
+++ player/Hand.java
@@ -1,20 +1,29 @@
 package player;
 
+import java.util.ArrayList;
 import java.util.List;
 import cards.Card;
 
 class Hand {
     List<Card> cardsToUse;
+    UsedPile usedPileCards;
 
-    Hand(){
-
+    Hand(UsedPile usedPile){
+        cardsToUse = new ArrayList<Card>();
+        usedPileCards = usedPile;
     }
 
     void moveToUsedPile(Card card){
-
+        usedPileCards.addToUsedPile(card);
     }
 
     Card removeCard(){
-        return null;
+        Card returnCard = cardsToUse.get(0);
+        cardsToUse.remove(returnCard);
+        return returnCard;
+    }
+
+    void setListOfCards(List<Card> newListOfCard){
+        cardsToUse = newListOfCard;
     }
 }
