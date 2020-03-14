9ff8b42ae44602e39d57577bd3f1d3703c3b293b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-14 12:07:01 +0100

    Implement method in usedPile

--- player/UsedPile.java
+++ player/UsedPile.java
@@ -1,13 +1,25 @@
 package player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cards.Card;
 
 class UsedPile{
     List<Card> usedCards;
+    Integer count;
 
-    public UsedPile(){
-        
+    UsedPile(){
+        usedCards = new ArrayList<Card>();
+        count = 0;
+    }
+
+    void addToUsedPile(Card newCard){
+        usedCards.add(newCard);
+        count++;
+    }
+
+    int countOfCards(){
+        return count;
     }
 }
