18af71ab241936bc8a56023f8bc7ffa3b5b57578
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-18 14:52:26 +0100

    Refactor comparator class

--- cards/CardIncubationComparator.java
+++ cards/CardIncubationComparator.java
@@ -5,7 +5,12 @@ public class CardIncubationComparator implements Comparator<Card>{
 
     @Override
     public int compare(Card card1, Card card2) {
-        return card1.getIncubation() - card2.getIncubation();
+        int result = card1.getIncubation() - card2.getIncubation();
+        if(result > 0)
+            return 1;
+        if(result < 0)
+            return -1;
+        return result;
     }
     
 }
--- cards/CardPainfulnessComparator.java
+++ cards/CardPainfulnessComparator.java
@@ -6,7 +6,12 @@ public class CardPainfulnessComparator implements Comparator<Card>{
 
     @Override
     public int compare(Card card1, Card card2) {
-        return card1.getPainfulness() - card2.getPainfulness();
+        int result = card1.getPainfulness() - card2.getPainfulness();
+        if(result > 0)
+            return 1;
+        if(result < 0)
+            return -1;
+        return result;
     }
     
 }
