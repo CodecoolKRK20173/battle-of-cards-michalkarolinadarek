839e105185e8ac34d5996663d1dc647200401bf3
Dariusz Raba <rabadariusz@gmail.com>
2020-03-21 16:19:38 +0100

    Create xml file

---
+++ src/main/java/com/Lechowicz/apps/deck/DeckDAOxml.java
@@ -0,0 +1,58 @@
+package com.Lechowicz.apps.deck;
+
+import com.Lechowicz.apps.cards.Card;
+
+import java.util.List;
+
+public class DeckDAOxml implements  DeckDAOInterface{
+
+    public DeckDAOxml(){
+
+    }
+    private void openFile(){
+
+    }
+    private void loadAllCardsFromFile(){
+
+    }
+
+    @Override
+    public List<Card> getDeck() {
+        return null;
+    }
+
+    @Override
+    public Card getCard(int index) {
+        return null;
+    }
+
+    @Override
+    public void updateDeck(List<Card> deck) {
+
+    }
+
+    @Override
+    public void updateCard(Card card, int index) {
+
+    }
+
+    @Override
+    public void deleteCard(Card card) {
+
+    }
+
+    @Override
+    public void deleteCard(int index) {
+
+    }
+
+    @Override
+    public void deleteDeck() {
+
+    }
+
+    @Override
+    public void addCard(Card card) {
+
+    }
+}
